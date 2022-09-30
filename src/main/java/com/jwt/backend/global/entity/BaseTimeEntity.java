b8ca6c4030da87b3edeef9ee04bc9bd3d5118f9b
cloudwi <cloudwi@naver.com>
2022-10-01 00:57:33 +0900

    @EnableJpaAuditing add

--- src/main/java/com/jwt/backend/global/entity/BaseTimeEntity.java
+++ src/main/java/com/jwt/backend/global/entity/BaseTimeEntity.java
@@ -6,7 +6,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -18,7 +17,6 @@ public class BaseTimeEntity {
 
     @CreatedDate
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(updatable = false)
     private LocalDateTime createDate;
 
     @LastModifiedDate
