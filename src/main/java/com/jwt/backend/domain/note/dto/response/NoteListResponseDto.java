a4ed5ce623e4d60dfd6e0f40a6348f1e0fb868bf
cloudwi <cloudwi@naver.com>
2022-10-04 21:22:37 +0900

    this.content = note.getContent(); delete

--- src/main/java/com/jwt/backend/domain/note/dto/response/NoteListResponseDto.java
+++ src/main/java/com/jwt/backend/domain/note/dto/response/NoteListResponseDto.java
@@ -14,14 +14,12 @@ import java.time.LocalDateTime;
 public class NoteListResponseDto {
     private Long id;
     private String title;
-    private String content;
     private String importance;
     private LocalDateTime modifiedDate;
 
     public NoteListResponseDto(Note note) {
         this.id = note.getId();
         this.title = note.getTitle();
-        this.content = note.getContent();
         this.importance = note.getImportance();
         this.modifiedDate = note.getModifiedDate();
     }
